dbc3441a17f13bd6fca61970a1755f004c70d7bd
ytyaru0 <ytyaru0@outlook.jp>
2018-03-15 14:02:44 +0900

    比較式の追加

--- res/sh/syntax/comparison.sh
+++ res/sh/syntax/comparison.sh
@@ -17,3 +17,23 @@ fi
 test 'A' == 'A'; echo $?
 test 'A' != 'B'; echo $?
 # ファイル更新日時
+NEW=/tmp/work/new.txt
+OLD=/tmp/work/old.txt
+touch -d "1999/12/31 23:59:59" $OLD
+touch -d "2000/01/01 00:00:00" $NEW
+test $NEW -nt $OLD; echo $?
+test $OLD -ot $NEW; echo $?
+[ $NEW -nt $OLD ]; echo $?
+[ $OLD -ot $NEW ]; echo $?
+test -z ""; echo $?
+test -n "A"; echo $?
+test -d $(dirname $NEW); echo $?
+test -f $NEW; echo $?
+echo 'A' > $NEW
+test -s $NEW; echo $?
+test -e $NEW; echo $?
+chmod 755 $NEW
+test -r $NEW; echo $?
+test -w $NEW; echo $?
+test -x $NEW; echo $?
+
