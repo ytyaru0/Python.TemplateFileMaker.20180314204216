3be7321f24add80db96ca4e7e98831f0ceac8f2d
ytyaru0 <ytyaru0@outlook.jp>
2018-03-21 21:32:12 +0900

    テンプレ追加。py関数

--- res/py/3/Syntax/function/arguments.py
+++ res/py/3/Syntax/function/arguments.py
@@ -1,3 +1,22 @@
+def func0(): return 0
+print(func0())
+
+def func1(arg): return arg
+print(func1('A'))
+
+def func2(arg='B'): return arg
+print(func2())
+
+def func3(a, b): return a + b
+print(func3('A', 'B'))
+
+def func4(a, b='b'): return a + b
+print(func4('A', b='B'))
+
+def func5(): print('func5')
+r = func5()
+print('func5 return is {}'.format(r))
+
 def func(*args, **argv):
     print(args, argv)
 
@@ -10,6 +29,7 @@ func(1, 2, name='A', age=4)
 l = [1, 2, 3]
 d = {'name': 'A', 'age': 4}
 func(*l, **d)
+func(*[1, 2, 3], **{'name': 'A', 'age': 4})
 
 l = list((1, 2, 3))
 d = dict(name='A', age=4)
