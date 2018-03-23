389b1b55dc8290f46a04aac6c4c16b61a0330439
ytyaru0 <ytyaru0@outlook.jp>
2018-03-23 13:40:54 +0900

    デフォルトファイルパス

--- src/ConfigFile.py
+++ src/ConfigFile.py
@@ -7,7 +7,9 @@ class ConfigFile:
         self.__path_dir_res = self.__path_dir_root / 'res'
         if file_name.endswith('.tsv'): self.__file_name = file_name
         else: self.__file_name = file_name + '.tsv'
-        self.__LoadCommandsDirPathFromMetaFile()
+        self.__GetDefaultPath()
+        self.__LoadFilePath()
+        self.__LoadTemplateDir()
     
     @property
     def FilePath(self): return self.__path_file_this
@@ -16,14 +18,16 @@ class ConfigFile:
     @property
     def DefaultFilePath(self): return self.__path_dir_res / self.__file_name
 
-    def __LoadCommandsDirPathFromMetaFile(self):
+    def __LoadFilePath(self):
         work_paths = self.__LoadMetaPath('work')
-        root_paths = self.__LoadMetaPath('root')
         self.__path_file_this = pathlib.Path(work_paths['work_meta_command_do']) / self.__file_name
+        self.__path_file_this.parent.mkdir(parents=True, exist_ok=True)
+
+    def __LoadTemplateDir(self):
+        root_paths = self.__LoadMetaPath('root')
         self.__path_dir_template = pathlib.Path(root_paths['root_db_template']) / self.__file_name
         if not self.__path_dir_template.is_dir():
             self.__path_dir_template = self.__path_dir_res
-        self.__path_file_this.parent.mkdir(parents=True, exist_ok=True)
 
     def __LoadMetaPath(self, file_name):
         path_config = self.__GetMetaPath(file_name)
@@ -40,6 +44,13 @@ class ConfigFile:
             if path.is_file(): return path
         raise Exception('{}.iniファイルが存在しません。次のいずれかのディレクトリ配下に作成してください'.format(file_name))
         return None
+
+    def __GetDefaultPath(self):
+        paths = [self.__path_dir_root / 'config', self.__path_dir_root, self.__path_dir_res]
+        for path in paths:
+            p = path / self.__file_name
+            if p.is_file(): return p
+        raise Exception('{} デフォルトファイルが存在しません。次のパス配下に作成してください。'.format(self.__file_name, paths))
 
     def LoadTsv(self):
         with self.__path_file_this.open() as f:
