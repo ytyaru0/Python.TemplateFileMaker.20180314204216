db7db549ed7e4866691fef0b06a16e2c777211ac
ytyaru0 <ytyaru0@outlook.jp>
2018-03-24 16:20:17 +0900

    テンプレカテゴリとテンプレ引数の分割用意

--- src/GetTemplateFilePath.py
+++ src/GetTemplateFilePath.py
@@ -1,4 +1,32 @@
+import sys
+import os.path
+from CommandsFile import CommandsFile
+
+class TemplateFilePath:
+    def Get(self, args):
+        categolies, tmpl_vars = self.__SplitCategolyAndTemplateVars(args)
+        input_command = ' '.join([a for a in categolies]).strip()
+        f = CommandsFile()
+        for d in f.Load():
+            for c in d.commands:
+                if c == input_command:
+                    return os.path.join(f.TemplateDir, d.path)
+        return None
+    def __SplitCategolyAndTemplateVars(self, args):
+        for i, a in enumerate(args):
+            if a.startswith('-'): return args[:i], args[i:]
+        return args, []
+    def __IsKeyword(self, tmpl_args):
+        for a in tmpl_args:
+            if not a.startswith('-'): return True
+        return False
+
 if __name__ == '__main__':
+    path = TemplateFilePath().Get(sys.argv[1:])
+    if path: print(path)
+    else: raise Exception('<ERROR> 指定したコマンドに該当するテンプレートファイルパスは存在しません。')
+    """
+    print(t.Get(sys.argv[1:])
     import sys
     import os.path
     from CommandsFile import CommandsFile
@@ -12,4 +40,5 @@ if __name__ == '__main__':
                 print(os.path.join(f.TemplateDir, d.path))
                 sys.exit()
     raise Exception('<ERROR> 指定したコマンドに該当するテンプレートファイルパスは存在しません。')
+    """
 
